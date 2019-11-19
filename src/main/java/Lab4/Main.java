a33b0541972c059ff0a3f9f2177a67b3214f70f7
GromkovAlexander <gromkovalex@mail.ru>
2019-11-19 21:54:22 +0300

    Scripted auto-commit on change (2019-11-19 21:54:22) by gitwatch.sh

--- src/main/java/Lab4/Main.java
+++ src/main/java/Lab4/Main.java
@@ -8,9 +8,8 @@ public class Main {
 
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("test");
-        ActorRef storeActor = system.actorOf(
-                Props.create(StoreActor.class)
-        );
+        ActorRef storeActor = system.actorOf(Props.create(StoreActor.class));
+        
         storeActor.tell(
                 new StoreActor.StoreMessage("test", "test"),
                 ActorRef.noSender()

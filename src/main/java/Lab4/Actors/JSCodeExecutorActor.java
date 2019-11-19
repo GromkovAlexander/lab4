e7d26ef91f131b2a22423ea7811987def12e4850
GromkovAlexander <gromkovalex@mail.ru>
2019-11-19 23:08:03 +0300

    Scripted auto-commit on change (2019-11-19 23:08:03) by gitwatch.sh

--- src/main/java/Lab4/Actors/JSCodeExecutorActor.java
+++ src/main/java/Lab4/Actors/JSCodeExecutorActor.java
@@ -1,10 +1,22 @@
 package Lab4.Actors;
 
+import Lab4.Packages.PackageInputJS;
+import Lab4.Packages.RunningMessage;
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+
+import javafx.util.Pair;
+
 
 public class JSCodeExecutorActor extends AbstractActor {
     @Override
     public Receive createReceive() {
-        return null;
+        return ReceiveBuilder.create()
+                .match(
+                        RunningMessage.class, m -> {
+                            
+                        }
+                )
+                .build();
     }
 }

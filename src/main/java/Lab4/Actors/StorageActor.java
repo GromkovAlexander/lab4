76aa1ae7b44f253d40396ac436da4e6c119201c6
GromkovAlexander <gromkovalex@mail.ru>
2019-11-19 23:37:45 +0300

    Scripted auto-commit on change (2019-11-19 23:37:45) by gitwatch.sh

--- src/main/java/Lab4/Actors/StorageActor.java
+++ src/main/java/Lab4/Actors/StorageActor.java
@@ -1,11 +1,24 @@
 package Lab4.Actors;
 
+import Lab4.Packages.PackageInputJS;
+import Lab4.Packages.StorrageMessage;
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+
+import java.util.Map;
 
 public class StorageActor extends AbstractActor {
 
+    private Map<Integer, PackageInputJS> storage = new Map<>();
+
     @Override
     public Receive createReceive() {
-        return null;
+        return ReceiveBuilder.create()
+                .match(
+                        StorrageMessage.class, msg -> {
+
+                        }
+                )
+                .build();
     }
 }

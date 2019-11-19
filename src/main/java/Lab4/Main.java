6180092ed98605d24f1810b9cf421ba3e92175f8
GromkovAlexander <gromkovalex@mail.ru>
2019-11-19 22:06:43 +0300

    Scripted auto-commit on change (2019-11-19 22:06:43) by gitwatch.sh

--- src/main/java/Lab4/Main.java
+++ src/main/java/Lab4/Main.java
@@ -17,6 +17,11 @@ import java.util.concurrent.CompletionStage;
 
 public class Main {
 
+    private final static String ROUTES = "routes";
+    private final static String LOCALHOST = "localhost";
+    private final static int LOCALHOST_PORT = 8080;
+    private final static String SERVER_ONLINE_MESSAGE = "Server online at http://localhost:" + LOCALHOST_PORT + "/\nPress RETURN to stop...";
+
     public static void main(String[] args) throws IOException {
         ActorSystem system = ActorSystem.create("routes");
         ActorRef storeActor = system.actorOf(Props.create(StoreActros.class));
@@ -29,15 +34,17 @@ public class Main {
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = instance.createRoute(system).flow(system, materializer);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
-                ConnectHttp.toHost("localhost", 8080),
+                ConnectHttp.toHost(LOCALHOST, LOCALHOST_PORT),
                 materializer
         );
 
-        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.out.println(SERVER_ONLINE_MESSAGE);
         System.in.read();
 
         binding
                 .thenCompose(ServerBinding::unbind)
                 .thenAccept(unbound -> system.terminate());
     }
+
+
 }

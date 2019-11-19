8bed7f77428ec63999afb56eb498bcd0a4abef12
GromkovAlexander <gromkovalex@mail.ru>
2019-11-19 22:57:58 +0300

    Scripted auto-commit on change (2019-11-19 22:57:58) by gitwatch.sh

--- src/main/java/Lab4/Packages/RunningMessage.java
+++ src/main/java/Lab4/Packages/RunningMessage.java
@@ -1,4 +1,14 @@
 package Lab4.Packages;
 
+import javafx.util.Pair;
+
 public class RunningMessage {
+
+    private Pair<Integer, PackageInputJS> msg;
+
+    public RunningMessage(Pair<Integer, PackageInputJS> msg) {
+        this.msg = msg;
+    }
+
+
 }

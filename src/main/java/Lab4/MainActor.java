fc2539808536d0a2d4cef39251e06cabf868af3d
GromkovAlexander <gromkovalex@mail.ru>
2019-11-19 22:17:18 +0300

    Scripted auto-commit on change (2019-11-19 22:17:18) by gitwatch.sh

--- src/main/java/Lab4/MainActor.java
+++ src/main/java/Lab4/MainActor.java
@@ -1,4 +1,4 @@
 package Lab4;
 
-public class StoreActros {
+public class MainActor {
 }
--- src/main/java/Lab4/TesterJS.java
+++ src/main/java/Lab4/TesterJS.java
@@ -9,13 +9,14 @@ import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.server.Route;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
 import java.io.IOException;
 import java.util.concurrent.CompletionStage;
 
-public class Main {
+public class TesterJS {
 
     private final static String ROUTES = "routes";
     private final static String LOCALHOST = "localhost";
@@ -23,15 +24,15 @@ public class Main {
     private final static String SERVER_ONLINE_MESSAGE = "Server online at http://localhost:" + LOCALHOST_PORT + "/\nPress RETURN to stop...";
 
     public static void main(String[] args) throws IOException {
-        ActorSystem system = ActorSystem.create("routes");
-        ActorRef storeActor = system.actorOf(Props.create(StoreActros.class));
+        ActorSystem system = ActorSystem.create(ROUTES);
+        ActorRef mainActor = system.actorOf(Props.create(MainActor.class));
 
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
 
-        MainHttp instance = new MainHttp(system);
+        TesterJS testerJS = new TesterJS();
 
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = instance.createRoute(system).flow(system, materializer);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = testerJS.testJsRoute(mainActor).flow(system, materializer);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
                 ConnectHttp.toHost(LOCALHOST, LOCALHOST_PORT),
@@ -44,6 +45,10 @@ public class Main {
         binding
                 .thenCompose(ServerBinding::unbind)
                 .thenAccept(unbound -> system.terminate());
+    }
+
+    private Route testJsRoute(ActorRef mainActor) {
+        return ;
     }
 
 

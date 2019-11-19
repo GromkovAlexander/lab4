0f4d95b6b7f147ae0fa10f621f7b6ad71f0930d5
GromkovAlexander <gromkovalex@mail.ru>
2019-11-19 22:43:22 +0300

    Scripted auto-commit on change (2019-11-19 22:43:22) by gitwatch.sh

--- src/main/java/Lab4/Actors/MainActor.java
+++ src/main/java/Lab4/Actors/MainActor.java
@@ -3,6 +3,9 @@ package Lab4.Actors;
 import akka.actor.AbstractActor;
 
 public class MainActor extends AbstractActor {
+
+    
+
     @Override
     public Receive createReceive() {
         return null;

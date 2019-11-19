175b06169288690d4b98f045846111791ba6217b
GromkovAlexander <gromkovalex@mail.ru>
2019-11-19 21:53:33 +0300

    Scripted auto-commit on change (2019-11-19 21:53:33) by gitwatch.sh

--- src/main/java/Lab4/Main.java
+++ src/main/java/Lab4/Main.java
@@ -1,8 +1,19 @@
 package Lab4;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        ActorSystem system = ActorSystem.create("test");
+        ActorRef storeActor = system.actorOf(
+                Props.create(StoreActor.class)
+        );
+        storeActor.tell(
+                new StoreActor.StoreMessage("test", "test"),
+                ActorRef.noSender()
+        );
     }
 }

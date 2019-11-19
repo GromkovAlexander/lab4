1d6e8f1198504638157d05ee3739d43c1af9401e
GromkovAlexander <gromkovalex@mail.ru>
2019-11-19 22:28:21 +0300

    Scripted auto-commit on change (2019-11-19 22:28:21) by gitwatch.sh

--- src/main/java/Lab4/Packages/PackageInputJS.java
+++ src/main/java/Lab4/Packages/PackageInputJS.java
@@ -4,6 +4,27 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class PackageInputJS {
 
-    public PackageInputJS(@JsonProperty())
+    private final static String PACKAGE_ID = "packageId";
+    private final static String JS_SCRIPT = "jsScript";
+    private final static String FUNCTION_NAME = "functionName";
+    private final static String Tests = "tests";
+
+    private int packageId;
+    private String jsScript;
+    private String functionName;
+    private Test[] tests;
+
+    public PackageInputJS(@JsonProperty(PACKAGE_ID) String packageId,
+                          @JsonProperty(JS_SCRIPT) String jsScript,
+                          @JsonProperty(FUNCTION_NAME) String functionName,
+                          @JsonProperty(Tests) Test[] tests) {
+
+    }
+
+
+    
+
 
 }
+
+

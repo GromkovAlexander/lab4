5f64c7b03346e969fb9b4fc0fd41930601f3c56e
GromkovAlexander <gromkovalex@mail.ru>
2019-11-20 22:05:36 +0300

    Scripted auto-commit on change (2019-11-20 22:05:36) by gitwatch.sh

--- src/main/java/Lab4/Actors/StorageActor.java
+++ src/main/java/Lab4/Actors/StorageActor.java
@@ -6,75 +6,12 @@ import Lab4.Packages.StorrageMessage;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class StorageActor extends AbstractActor {
 
 
-    private Map<Integer, ArrayList<StorrageMessage>> storage = new Map<Integer, ArrayList<StorrageMessage>>() {
-        @Override
-        public int size() {
-            return 0;
-        }
-
-        @Override
-        public boolean isEmpty() {
-            return false;
-        }
-
-        @Override
-        public boolean containsKey(Object key) {
-            return false;
-        }
-
-        @Override
-        public boolean containsValue(Object value) {
-            return false;
-        }
-
-        @Override
-        public ArrayList<StorrageMessage> get(Object key) {
-            return null;
-        }
-
-        @Override
-        public ArrayList<StorrageMessage> put(Integer key, ArrayList<StorrageMessage> value) {
-            return null;
-        }
-
-        @Override
-        public ArrayList<StorrageMessage> remove(Object key) {
-            return null;
-        }
-
-        @Override
-        public void putAll(Map<? extends Integer, ? extends ArrayList<StorrageMessage>> m) {
-
-        }
-
-        @Override
-        public void clear() {
-
-        }
-
-        @Override
-        public Set<Integer> keySet() {
-            return null;
-        }
-
-        @Override
-        public Collection<ArrayList<StorrageMessage>> values() {
-            return null;
-        }
-
-        @Override
-        public Set<Entry<Integer, ArrayList<StorrageMessage>>> entrySet() {
-            return null;
-        }
-    };
+    private HashMap<Integer, ArrayList<StorrageMessage>> storage = new Map<Integer, ArrayList<StorrageMessage>>();
 
     @Override
     public Receive createReceive() {
